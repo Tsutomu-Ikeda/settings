8687d4194a5d44d4d4be8de50d060da2015d3732
Tsutomu Ikeda <tsutomu.ikeda@clipline.jp>
2019-04-05 15:47:01 +0900

    Update ~/.config

---
+++ ~/.config/fish/config.fish
@@ -0,0 +1,9 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+set -U FZF_LEGACY_KEYBINDINGS 0
+
+set -x PATH $HOME/.pyenv/bin $PATH
+. (pyenv init - | psub)

ffb7499bacb2309af06e784d8622c075d65c1b63
tomtsutom <tomtsutom0122@gmail.com>
2020-08-20 21:38:49 +0900

    docker-compose-devエイリアスを追加

--- unix/home/.zsh/alias.zsh
+++ unix/home/.zsh/alias.zsh
@@ -1,3 +1,4 @@
 alias rebirth='echo Reincarnate zsh... ; rm -rfv ~/.antigen/ ; [ ! -d ~/.antigen ] && mkdir -p ~/.antigen && curl -L git.io/antigen > ~/.antigen/antigen.zsh; source ~/.zshrc'
 alias nconsole='tmux new-s -s Console'
 alias restart=". ~/.zshrc"
+alias docker-compose-dev='docker-compose -f docker-compose.dev.yml'

dda818cd85010fca2bbdbae4ff9915adc020fb19
tomtsutom <tomtsutom0122@gmail.com>
2020-10-04 14:42:58 +0900

    Homebrewのパッケージ管理周りを整理

--- unix/home/.zsh/profile/macos.zsh
+++ unix/home/.zsh/profile/macos.zsh
@@ -2,15 +2,13 @@ alias ls='gls --color'
 alias ll='gls -lh --color'
 alias la='gls -lah --color'
 
+alias rm="rmtrash"
+
 export PATH="/usr/local/sbin:$PATH"
 
 export PATH="$HOME/.pyenv/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
-fi
-export PATH="$HOME/.rbenv/bin:$PATH"
-if command -v rbenv 1>/dev/null 2>&1; then
-  eval "$(rbenv init -)"
 fi
 
 alias brew="env PATH=${PATH/$HOME\/\.pyenv\/shims:/} brew"

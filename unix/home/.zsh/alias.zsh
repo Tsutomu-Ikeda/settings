1306abb13b0e6a36cb397bed5aa3a22f488aa23e
tomtsutom <40418321+Tsutomu-Ikeda@users.noreply.github.com>
2020-12-25 10:59:40 +0900

    ディレクトリ移動の際に戻る/進むを使えるようにした

--- unix/home/.zsh/alias.zsh
+++ unix/home/.zsh/alias.zsh
@@ -4,3 +4,41 @@ alias restart=". ~/.zshrc"
 alias docker-compose-dev='docker-compose -f docker-compose.dev.yml'
 alias tree="tree -N"
 alias less="less -R"
+alias codei='code-insiders'
+
+function cd {
+  builtin cd "$@"
+  if [[ $OLDPWD != $PWD ]]
+  then
+    BACK_HISTORY=$OLDPWD:$BACK_HISTORY
+    FORWARD_HISTORY=""
+  fi
+}
+
+function .. {
+  cd ..
+}
+
+function bd {
+  DIR=${BACK_HISTORY%%:*}
+  if [[ -d "$DIR" ]]
+  then
+    BACK_HISTORY=${BACK_HISTORY#*:}
+    FORWARD_HISTORY=$PWD:$FORWARD_HISTORY
+    builtin cd "$DIR"
+  else
+    echo "empty \$BACK_HISTORY"
+  fi
+}
+
+function fd {
+  DIR=${FORWARD_HISTORY%%:*}
+  if [[ -d "$DIR" ]]
+  then
+    FORWARD_HISTORY=${FORWARD_HISTORY#*:}
+    BACK_HISTORY=$PWD:$BACK_HISTORY
+    builtin cd "$DIR"
+  else
+    echo "empty \$FORWARD_HISTORY"
+  fi
+}

fc8381eedcf777267086af22cbe9cc93ba539579
tomtsutom <40418321+Tsutomu-Ikeda@users.noreply.github.com>
2024-04-10 14:21:54 +0000

    ssh-agentの設定を追加した

--- unix/home/.zsh/profile/linux.zsh
+++ unix/home/.zsh/profile/linux.zsh
@@ -1,3 +1,16 @@
 alias ls='ls --color'
 alias ll='ls -lh --color'
 alias la='ls -lah --color'
+
+SSH_AGENT_FILE=$HOME/.ssh-agent
+test -f $SSH_AGENT_FILE && source $SSH_AGENT_FILE
+
+function ssh() {
+  if ! ssh-add -l > /dev/null 2>&1; then
+    ssh-agent > $SSH_AGENT_FILE
+    source $SSH_AGENT_FILE
+    ssh-add -t 86400 $HOME/.ssh/id_rsa
+  fi
+
+  command ssh "$@"
+}
